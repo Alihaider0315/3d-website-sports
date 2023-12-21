782784bddf46ff40f31c9116d79d3e7a79183ca6
Alihaider0315 <ah712200@gmail.com>
2023-12-21 13:05:00 +0500

    3d-website

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const App = () => {
+  return (
+    <div>
+    <spline-viewer url="https://prod.spline.design/8t3QX78r1eQjSekT/scene.splinecode"></spline-viewer>
+    </div>
+  )
+}
+
+export default App
