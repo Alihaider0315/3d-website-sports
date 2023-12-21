80648c15c9f28fa2f40c47dcb0a21aa8bc544599
Alihaider0315 <ah712200@gmail.com>
2023-12-21 13:50:32 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-    <spline-viewer url="https://prod.spline.design/99MrbuISW5cGZ6Bw/scene.splinecode"></spline-viewer>
+    <iframe src='https://my.spline.design/elegantbeautyofdarkaesthetics-9a666456b68a91e14dab73263ce826d3/' frameborder='0' width='100%' height='1000px'></iframe>
     </div>
   )
 }
